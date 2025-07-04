b88a4c457214739be2c0dccfc19c8bbcade4ae29
samuel beau <samuel.beau.sasu@gmail.com>
2025-07-04 15:15:25 +0000

    correction cac40

--- definitions/generate_predictions.js
+++ definitions/generate_predictions.js
@@ -1,7 +1,7 @@
 // definitions/generate_predictions.js
 
 // Définir les indices sur lesquels nous voulons itérer
-const indices = ["DJI", "CAC40", "SP500"];
+const indices = ["DJI", "cac40", "SP500"];
 
 // Définir le nombre de paires de prédictions (y_proba_n, y_pred_n)
 const numPredictions = 10;
